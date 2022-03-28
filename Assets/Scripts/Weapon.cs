64e676e44c4959773d469148ef509dcc897e9b13
Steven Kennedy <umbrylax@gmail.com>
2022-03-28 11:14:57 -0600

    weapon moves toward mouse, to help accuracy

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -5,19 +5,33 @@ using UnityEngine;
 public class Weapon : MonoBehaviour
 {
 
-    private Vector3 offset = new Vector3(1, 1.22f, 0);
-    private GameObject player;
+
+    public GameObject bulletSpawnPoint;
+    private int clipCapacity;
+    private float reloadSpeed;
     // Start is called before the first frame update
     void Awake()
     {
-        player = GameObject.Find("Player");
+
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = player.transform.position + offset;
-        transform.rotation = player.transform.rotation;
+        WeaponMovement();
     }
+
+    private void WeaponMovement()
+    {
+        //rotates weapon towards the mouse
+        Vector3 mouseDifference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        mouseDifference.Normalize();
+        float roatationZ = (Mathf.Atan2(mouseDifference.y, mouseDifference.x) * Mathf.Rad2Deg) - 90f;
+
+
+        transform.rotation = Quaternion.Euler(0f, 0f, roatationZ);
+    }
+
+    void WeaponAttack() { }
 }
