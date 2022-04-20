a5050b309273df4329c492c5621c084435e8e8d6
Steven Kennedy <umbrylax@gmail.com>
2022-04-20 11:18:51 -0600

    Updated UI gameover, created taser for enemy fx

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,12 +6,13 @@ public class Enemy : MonoBehaviour
 {
     // Start is called before the first frame update
 
-    int enemyHealth = 20;
+    int enemyHealth = 40;
     bool isAttacking = false;
     public GameObject deathParticles;
-    void Start()
+    public GameObject taserElectricity;
+    void Awake()
     {
-
+        StartCoroutine(TaserEffectRoutine());
     }
 
     // Update is called once per frame
@@ -40,6 +41,8 @@ public class Enemy : MonoBehaviour
     {
         if (other.gameObject.CompareTag("PlayerBullet"))
         {
+            //currently with the two colliders, the enemy is taking double the damage displayed, as the bullet triggers both on impact. doubled health in the meantime to counter this. Later on will make the taser/attack a seperate script for convenience
+
             enemyHealth -= 5;
             Destroy(other.gameObject);
         }
@@ -53,6 +56,17 @@ public class Enemy : MonoBehaviour
         if (other.gameObject.CompareTag("Player"))
         {
             isAttacking = false;
+        }
+    }
+
+    IEnumerator TaserEffectRoutine()
+    {
+        while (!GameManager.gameOver)
+        {
+            taserElectricity.SetActive(true);
+            yield return new WaitForSeconds(.1f);
+            taserElectricity.SetActive(false);
+            yield return new WaitForSeconds(.1f);
         }
     }
 }
